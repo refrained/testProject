3a4a7af35d8eb8994b9023bad3990df2330d67ef
caofana <851505484@qq.com>
2020-03-02 15:51:38 +0800

    test

--- src/main/java/TestClass.java
+++ src/main/java/TestClass.java
@@ -1,5 +1,5 @@
 public class TestClass {
     public static void main(String[] args) {
-        System.out.println("hello testLProject!");
+        System.out.println("hello testLProject!!!!");
     }
 }
